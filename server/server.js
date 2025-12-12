b06c862b073823c9c21c19fa2c762b1590e9c153
kaibad <badukailash001@gmail.com>
2025-12-12 14:53:46 +0545

    express setup and folder structure

---
+++ server/server.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+import app from "./src/app.js";
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

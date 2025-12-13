cf8308103a77b427ab4dd4d3aa538f9863a089f9
kaibad <badukailash001@gmail.com>
2025-12-13 17:52:51 +0545

    fix: conflict error due to duplicate short key

--- server/src/services/url.service.js
+++ server/src/services/url.service.js
@@ -4,7 +4,7 @@ import { generateShortCode } from "../utils/helpers.js";
 export const createShortUrlServicewithoutUser = async (url) => {
   const shortCode = generateShortCode(7);
   if (!shortCode) throw new Error("Error generating unique id for this url");
-  await createShortUrlDao("uJ8zpTq", url);
+  await createShortUrlDao(shortCode, url);
 
   return shortCode;
 };
